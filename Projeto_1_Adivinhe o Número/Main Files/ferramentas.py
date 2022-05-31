77ad0a32acb3571998a04eb715b247ca1e2480ef
Felipe P. Sousa <51722165+felipe03paula@users.noreply.github.com>
2022-05-31 17:19:45 -0300

    atualizei e modularizei o jogo

--- Projeto_1_Adivinhe o Número/Main Files/ferramentas.py
+++ Projeto_1_Adivinhe o Número/Main Files/ferramentas.py
@@ -1,3 +1,4 @@
+# Modulos usados no jogo
 def aleatorio(a, b):
     from random import randint
     ale = randint(a, b)
@@ -33,3 +34,63 @@ def musica():
     pygame.mixer.music.load(".\Projeto_1_Adivinhe o Número\Lofi.mp3")
     pygame.mixer.music.play()
     
+
+def jogo(inicio, fim):
+    if fim == 10:
+        dificuldade = "Easy"
+    elif fim == 30:
+        dificuldade = "Normal"
+    elif fim == 50:
+        dificuldade = "Hard"
+    total_score = 0
+    partidas = 0
+    while True:
+            partidas += 1
+            print("\033[1;32m")
+            linha(f"Estou pensando em um número entre ({inicio} e {fim})")
+            print("Que número é esse?")
+            score = fim
+            pc = aleatorio(inicio, fim)
+            print(f"RESP {pc}")
+            player = int(input("-> "))
+            print("\033[m")
+            while player != pc:
+                if player > pc:
+                    titulo("\033[1;31mBeh! É menos!\033[m")
+                else:
+                    titulo("\033[1;31mBeh! É mais!\033[m")
+                if score == 0:
+                    titulo("\033[1;31mGame Over! Suas chances acabaram!\033[m")
+                    break
+                score -= 1
+                player = int(input("-> "))
+            if player == pc:
+                print("\033[1;32m")
+                print(f"Parabéns você acertou! Meu número era {pc}")
+                print(f"Você ganhou {score} pontos!")
+                print("\033[1;32m")
+            
+            total_score += score
+
+            continua = str(input("Quer jogar de novo? [S/N] -> ")).upper().strip()[0]
+            while continua not in "SN":
+                print("Erro! Por Favor responda apenas com [S/N] -> ")
+                cpmtinua = str(input("-> "))
+            if continua == "N":
+                print("OK")
+                break
+    banco = open(".\Projeto_1_Adivinhe o Número\pontuacoes.txt", "a")
+
+    if total_score > 0:
+        name = str(input("Digite seu nome: ")).title().strip()
+        print(f"Sua pontuação é {total_score} pontos!")
+
+        banco.write(f"Nome do Jogador: [{name}]")
+        banco.write("\t")
+        banco.write(str(f"Total Score: [{total_score}] "))
+        banco.write("\t")
+        banco.write(f"Dificuldade: [{dificuldade}]")
+        banco.write("\t")
+        banco.write(f"Number of Times: [{partidas}]")
+        banco.write("\n")
+
--- Projeto_1_Adivinhe o Número/Main Files/jogo_adivinhe_o_numero.py
+++ Projeto_1_Adivinhe o Número/Main Files/jogo_adivinhe_o_numero.py
@@ -1,151 +1,35 @@
 import ferramentas
 
 name = " "
-total_score = 0
-dificuldade = " "
 partidadas = 0
 
 ferramentas.musica()
 
 ferramentas.prin("Advinhe o número!")
+
 print("\033[1;32mGostaria de Jogar? [S/N]")
 menu0 = str(input("-> ")).strip().upper()[0]
+
 while menu0 not in "SN":
     menu0 = str(input("-> ")).strip().upper()[0]
+
 if menu0 == "S":
     print("Ótimo")
     print('''\033[1mEscolha uma dificuldade:
     -----------\033[m
-    \033[1;32m1. Fácil\033[m
-    \033[1;33m2. Médio\033[m
-    \033[1;31m3. Difícil\033[m''')
+    \033[1;32m1. Fácil (Easy)\033[m
+    \033[1;33m2. Médio (Normal)\033[m
+    \033[1;31m3. Difícil (Hard)\033[m''')
     menu1 = int(input("-> "))
 
     if menu1 == 1:
-        dificuldade = "Fácil"
-        while True:
-            partidadas += 1
-            print("\033[1;32m")
-            ferramentas.linha("Estou pensando em um número entre (1 e 10)")
-            print("Que número é esse?")
-            score = 10
-            pc = ferramentas.aleatorio(1, 10)
-            #print(f"RESP {pc}")
-            player = int(input("-> "))
-            print("\033[m")
-            while player != pc:
-                if player > pc:
-                    ferramentas.titulo("\033[1;31mBeh! É menos!\033[m")
-                else:
-                    ferramentas.titulo("\033[1;31mBeh! É mais!\033[m")
-                if score == 0:
-                    ferramentas.titulo(
-                        "\033[1;31mGame Over! Suas chances acabaram!\033[m")
-                    break
-                score -= 1
-                player = int(input("-> "))
-            if player == pc:
-                print("\033[1;32m")
-                print(f"Parabéns você acertou! Meu número era {pc}")
-                print(f"Você ganhou {score} pontos!")
-                print("\033[1;32m")
-            total_score += score
-            continua = str(
-                input("Quer jogar de novo? [S/N] -> ")).upper().strip()[0]
-            while continua not in "SN":
-                print("Erro! Por Favor responda apenas com [S/N] -> ")
-            if continua == "N":
-                print("OK")
-                break
+        ferramentas.jogo(1, 10)
 
-    if menu1 == 2:
-        dificuldade = "Médio"
-        while True:
-            partidadas += 1
-            print("\033[1;32m")
-            ferramentas.linha("Estou pensando em um número entre (1 e 30)")
-            print("Que número é esse?")
-            score = 30
-            pc = ferramentas.aleatorio(1, 30)
-            #print(f"RESP {pc}")
-            player = int(input("-> "))
-            print("\033[m")
-            while player != pc:
-                if player > pc:
-                    ferramentas.titulo("\033[1;31mBeh! É menos!\033[m")
-                else:
-                    ferramentas.titulo("\033[1;31mBeh! É mais!\033[m")
-                if score == 0:
-                    ferramentas.titulo(
-                        "\033[1;31mGame Over! Suas chances acabaram!\033[m")
-                    break
-                score -= 1
-                player = int(input("-> "))
-            if player == pc:
-                print("\033[1;32m")
-                print(f"Parabéns você acertou! Meu número era {pc}")
-                print(f"Você ganhou {score} pontos!")
-                print("\033[1;32m")
-            total_score += score
-            continua = str(
-                input("Quer jogar de novo? [S/N] -> ")).upper().strip()[0]
-            while continua not in "SN":
-                print("Erro! Por Favor responda apenas com [S/N] -> ")
-            if continua == "N":
-                print("OK")
-                break
+    elif menu1 == 2:
+        ferramentas.jogo(1, 30)
 
-    if menu1 == 3:
-        dificuldade = "Dificíl"
-        while True:
-            partidadas += 1
-            print("\033[1;32m")
-            ferramentas.linha("Estou pensando em um número entre (1 e 50)")
-            print("Que número é esse?")
-            score = 50
-            pc = ferramentas.aleatorio(1, 50)
-            #print(f"RESP {pc}")
-            player = int(input("-> "))
-            print("\033[m")
-            while player != pc:
-                if player > pc:
-                    ferramentas.titulo("\033[1;31mBeh! É menos!\033[m")
-                else:
-                    ferramentas.titulo("\033[1;31mBeh! É mais!\033[m")
-                if score == 0:
-                    ferramentas.titulo(
-                        "\033[1;31mGame Over! Suas chances acabaram!\033[m")
-                    break
-                score -= 1
-                player = int(input("-> "))
-            if player == pc:
-                print("\033[1;32m")
-                print(f"Parabéns você acertou! Meu número era {pc}")
-                print(f"Você ganhou {score} pontos!")
-                print("\033[1;32m")
-            total_score += score
-            continua = str(
-                input("Quer jogar de novo? [S/N] -> ")).upper().strip()[0]
-            while continua not in "SN":
-                print("Erro! Por Favor responda apenas com [S/N] -> ")
-            if continua == "N":
-                print("OK")
-                break
+    elif menu1 == 3:
+        ferramentas.jogo(1, 50)
 
 else:
     print("OK")
-
-banco = open("pontuacoes.txt", "a")
-
-if total_score > 0:
-    name = str(input("Digite seu nome: ")).title().strip()
-    print(f"Sua pontuação é {total_score} pontos!")
-
-    banco.write(f"Nome do Jogaor: [{name}]")
-    banco.write("\t")
-    banco.write(str(f"Pontuação total: [{total_score}] pontos"))
-    banco.write("\t")
-    banco.write(f"Dificuldade: [{dificuldade}]")
-    banco.write("\t")
-    banco.write(f"Número de partidas: [{partidadas}]")
-    banco.write("\n")
